e4ac792501cf61376445fdc00277e800a9c52171
Jenn Wheeler <jawheeler@wm.edu>
2020-04-20 14:50:16 -0400

    start breaking things out into separate functions

--- src/oracle/result.cr
+++ src/oracle/result.cr
@@ -22,61 +22,56 @@ module Oracle
   end
 
   class ResultSet < DB::ResultSet
+    @col_index : UInt32
+    @col_names : Array(String)
     @data_buffer : Pointer(ODPI::DpiData)
-    @names : Array(String)
+    @error_info : ODPI::DpiErrorInfo
     @num_cols : UInt32
+    @raw_context : ODPI::DpiContext*
+    @raw_stmt : ODPI::DpiStmt*
     @rows_affected : UInt64
 
     def initialize(statement : Statement, @num_cols)
       super(statement)
 
+      @col_index = 1
       @data_buffer = Pointer(ODPI::DpiData).null
-      @names = Array(String).new
+      @col_names = Array(String).new
 
-      raw_context = statement.connection.raw_context
-      error = ODPI::DpiErrorInfo.new
+      @raw_context = statement.connection.raw_context
+      @raw_stmt = statement.raw_stmt
+      @error_info = ODPI::DpiErrorInfo.new
 
-      if ODPI.dpi_stmt_get_row_count(statement.raw_stmt, out @rows_affected) != 0
-        ODPI.dpi_context_get_error(raw_context, pointerof(error))
-        error_msg = String.new(error.message)
+      if ODPI.dpi_stmt_get_row_count(@raw_stmt, out @rows_affected) != 0
+        ODPI.dpi_context_get_error(@raw_context, pointerof(@error_info))
+        error_msg = String.new(@error_info.message)
         raise "Error fetching number of rows affected: #{error_msg}"
       end
 
       query_info = ODPI::DpiQueryInfo.new
       (1..@num_cols).each do |i|
-        if ODPI.dpi_stmt_get_query_info(statement.raw_stmt, i, pointerof(query_info)) != 0
-          ODPI.dpi_context_get_error(raw_context, pointerof(error))
-          error_msg = String.new(error.message)
+        if ODPI.dpi_stmt_get_query_info(@raw_stmt, i, pointerof(query_info)) != 0
+          ODPI.dpi_context_get_error(@raw_context, pointerof(@error_info))
+          error_msg = String.new(@error_info.message)
           raise "Error fetching column names: #{error_msg}"
         end
 
         s = Slice.new(query_info.name, query_info.nameLength)
-        @names.push(String.new(s))
+        @col_names.push(String.new(s))
+      end
+    end
+
+    protected def fetch_next_field
+      res = ODPI.dpi_stmt_get_query_value(@raw_stmt, @col_index, out typenum,
+                                          pointerof(@data_buffer))
+
+      if res != 0
+        ODPI.dpi_context_get_error(@raw_context, pointerof(@error_info))
+        error_msg = String.new(@error_info.message)
+        raise "Error fetching column #{@col_index}: #{error_msg}"
       end
 
-      if ODPI.dpi_stmt_fetch(statement.raw_stmt, out found, out index) != 0
-        ODPI.dpi_context_get_error(raw_context, pointerof(error))
-        error_msg = String.new(error.message)
-        raise "Error fetching rows: #{error_msg}"
-      end
-
-      if found != 1
-        return
-      end
-
-      (1..@num_cols).each do |i|
-        res = ODPI.dpi_stmt_get_query_value(statement.raw_stmt, i,
-                                            out typenum, pointerof(@data_buffer))
-
-        if res != 0
-          ODPI.dpi_context_get_error(raw_context, pointerof(error))
-          error_msg = String.new(error.message)
-          raise "Error fetching column #{i}: #{error_msg}"
-        end
-
-        field = Field.new("", @data_buffer, typenum)
-        puts field.value
-      end
+      Field.new("", @data_buffer, typenum)
     end
 
     def column_count : Int32
@@ -84,15 +79,25 @@ module Oracle
     end
 
     def column_name(index : Int32) : String
-      @names[index]
+      @col_names[index]
     end
 
     def move_next : Bool
-      false
+      if ODPI.dpi_stmt_fetch(@raw_stmt, out found, out index) != 0
+        ODPI.dpi_context_get_error(@raw_context, pointerof(@error_info))
+        error_msg = String.new(@error_info.message)
+        raise "Error fetching rows: #{error_msg}"
+      end
+
+      if found != 1
+        false
+      else
+        true
+      end
     end
 
     def read
-      value = @buffer[@col_index]
+      value = fetch_next_field
       @col_index += 1
       value
     end
